1e1c8e8e15c9f7d573e8a0bd133ecb80a0d95daa
Icewater-661 <icewater661@foxmail.com>
2026-05-06 23:21:22 +0800

    修改 上升气流
    上升气流修改效果 改为0c白卡技能 所有处于浮空的单位额外获得1层飞行，否则获得10点失衡，升级后获得保留

--- CrimsonScepter_Angelina_ModCode/Cards/Updraft.cs
+++ CrimsonScepter_Angelina_ModCode/Cards/Updraft.cs
@@ -1,67 +1,63 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using CrimsonScepter_Angelina_Mod.CrimsonScepter_Angelina_ModCode.Abstracts;
+using CrimsonScepter_Angelina_Mod.CrimsonScepter_Angelina_ModCode.Helpers;
 using CrimsonScepter_Angelina_Mod.CrimsonScepter_Angelina_ModCode.Powers;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
-using MegaCrit.Sts2.Core.Models.Cards;
 
 namespace CrimsonScepter_Angelina_Mod.CrimsonScepter_Angelina_ModCode.Cards;
 
-/// <summary>
-/// 卡牌名：上升气流
-/// 费用：2
-/// 稀有度：罕见
-/// 卡牌类型：能力
-/// 效果：回合开始时，获得1层临时飞行。若你因受到攻击脱离浮空，向抽牌堆内添加一张眩晕。
-/// 升级后效果：减1费。
-/// </summary>
 public sealed class Updraft : AngelinaCard
 {
-    // 动态变量：上升气流的基础层数。
+    public override IEnumerable<CardKeyword> CanonicalKeywords => IsUpgraded ? [CardKeyword.Retain] : [];
+
     protected override IEnumerable<DynamicVar> CanonicalVars =>
     [
-        new PowerVar<UpdraftPower>(1m)
+        new PowerVar<ImbalancePower>(10m)
     ];
 
-    // 额外悬浮说明：
-    // 1. 临时飞行
-    // 2. 眩晕
-    // 3. 上升气流
     protected override IEnumerable<IHoverTip> ExtraHoverTips =>
     [
-        HoverTipFactory.FromPower<TemporaryFlyPower>(),
-        HoverTipFactory.FromCard<Dazed>(),
-        HoverTipFactory.FromPower<UpdraftPower>()
+        HoverTipFactory.FromPower<FlyPower>(),
+        HoverTipFactory.FromPower<ImbalancePower>()
     ];
 
-    // 初始化卡牌的基础信息：2费、能力、罕见、目标为自己。
     public Updraft()
-        : base(2, CardType.Power, CardRarity.Uncommon, TargetType.Self)
+        : base(0, CardType.Skill, CardRarity.Common, TargetType.Self)
     {
     }
 
-    // 打出时，对自己施加上升气流。
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
-        // 施法表现：播放施法动作。
-        await CreatureCmd.TriggerAnim(base.Owner.Creature, "Cast", base.Owner.Character.CastAnimDelay);
+        _ = choiceContext;
+        _ = cardPlay;
 
-        // 对自己施加上升气流。
-        await PowerCmd.Apply<UpdraftPower>(
-            base.Owner.Creature,
-            base.DynamicVars["UpdraftPower"].BaseValue,
-            base.Owner.Creature,
-            this
-        );
+        foreach (Creature creature in base.CombatState?.Creatures ?? Enumerable.Empty<Creature>())
+        {
+            if (!creature.IsAlive)
+            {
+                continue;
+            }
+
+            if (AirborneHelper.IsAirborne(creature))
+            {
+                await PowerCmd.Apply<FlyPower>(creature, 1m, base.Owner.Creature, this);
+            }
+            else
+            {
+                await PowerCmd.Apply<ImbalancePower>(creature, base.DynamicVars["ImbalancePower"].BaseValue, base.Owner.Creature, this);
+            }
+        }
     }
 
-    // 升级后仅减少1点费用。
     protected override void OnUpgrade()
     {
-        base.EnergyCost.UpgradeBy(-1);
+        AddKeyword(CardKeyword.Retain);
     }
 }
