35088b3e4c58e21bceef27f0cc93b67de16af238
Icewater-661 <icewater661@foxmail.com>
2026-05-11 21:26:51 +0800

    添加卡牌：纪念品
    （打赢复活赛！以前那个很多牌太意义不明了）纪念品：1c稀有技能：从3张（升级后：已升级的）其他角色的稀有卡选择一张加入手牌中，其在本回合可以免费打出，消耗。

---
+++ CrimsonScepter_Angelina_ModCode/Cards/Souvenir.cs
@@ -0,0 +1,78 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CrimsonScepter_Angelina_Mod.CrimsonScepter_Angelina_ModCode.Abstracts;
+using CrimsonScepter_Angelina_Mod.CrimsonScepter_Angelina_ModCode.Character;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Factories;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Nodes.CommonUi;
+
+namespace CrimsonScepter_Angelina_Mod.CrimsonScepter_Angelina_ModCode.Cards;
+
+public sealed class Souvenir : AngelinaCard
+{
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
+
+    public Souvenir()
+        : base(1, CardType.Skill, CardRarity.Rare, TargetType.Self)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        _ = cardPlay;
+
+        List<CardModel> options = CardFactory.GetDistinctForCombat(
+                base.Owner,
+                GetOtherCharacterRareCards(),
+                3,
+                base.Owner.RunState.Rng.CombatCardGeneration)
+            .ToList();
+
+        if (options.Count == 0)
+        {
+            return;
+        }
+
+        if (base.IsUpgraded)
+        {
+            CardCmd.Upgrade(options, CardPreviewStyle.None);
+        }
+
+        CardModel? selectedCard = await CardSelectCmd.FromChooseACardScreen(choiceContext, options, base.Owner);
+        if (selectedCard == null)
+        {
+            return;
+        }
+
+        selectedCard.SetToFreeThisTurn();
+        await CardPileCmd.AddGeneratedCardToCombat(selectedCard, PileType.Hand, addedByPlayer: true);
+    }
+
+    protected override void OnUpgrade()
+    {
+    }
+
+    private IEnumerable<CardModel> GetOtherCharacterRareCards()
+    {
+        return GetOtherCharacterPools()
+            .SelectMany(pool => pool.GetUnlockedCards(base.Owner.UnlockState, base.Owner.RunState.CardMultiplayerConstraint))
+            .Where(card =>
+                card.Rarity == CardRarity.Rare &&
+                card.CanBeGeneratedInCombat &&
+                card.Pool is not AngelinaCardPool);
+    }
+
+    private static IEnumerable<CardPoolModel> GetOtherCharacterPools()
+    {
+        yield return ModelDb.CardPool<IroncladCardPool>();
+        yield return ModelDb.CardPool<SilentCardPool>();
+        yield return ModelDb.CardPool<DefectCardPool>();
+        yield return ModelDb.CardPool<NecrobinderCardPool>();
+        yield return ModelDb.CardPool<RegentCardPool>();
+    }
+}
